0ddaca77515044d77455de089bdd56c7d991a5ee
danilocalixto77 <danilocalixto@gmail.com>
2023-07-02 18:17:04 -0400

    commit001

---
+++ teste.js
@@ -0,0 +1,39 @@
+/*
+function calcularDiferencaEmDias(data1, data2) {
+    // Converter as datas para objetos Date
+    var d1 = new Date(data1);
+    var d2 = new Date(data2);
+  
+    // Calcular a diferença em milissegundos
+    var diff = Math.abs(d2 - d1);
+  
+    console.log(diff)
+
+    // Converter a diferença para dias
+    var diffDias = Math.ceil(diff / (1000 * 60 * 60 * 24));
+  
+    return diffDias;
+  }
+  
+  // Exemplo de uso:
+  var dataInicial = '2023-05-08';
+  var dataFinal = '2023-06-28';
+  
+  var diferencaDias = calcularDiferencaEmDias(dataInicial, dataFinal);
+  console.log('A diferença em dias é:', diferencaDias);
+  
+
+
+
+function sayHi() {
+  console.log(name);
+  console.log(age);
+  var name = 'Lydia';
+  let age = 21;
+  }
+
+  sayHi()
+*/
+
+
+  
